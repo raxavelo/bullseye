5a44493fce82af761da343974f4b97ec9ad33cc8
raxavelo <raxavelo@gmail.com>
2022-01-20 18:53:39 +0100

    Added label, buttons and slider.

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var currentValue: Int = 50
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -15,18 +16,24 @@ class ViewController: UIViewController {
   }
 
   @IBAction func showAlert() {
+    let message = "The value of the slider is: \(currentValue)"
+    
     let alert = UIAlertController(
       title: "Hello, World",
-      message: "This is my first app!",
+      message: message,
       preferredStyle: .alert)
     
     let action = UIAlertAction(
-    title: "Awesome",
+    title: "OK",
     style: .default,
     handler: nil)
     
     alert.addAction(action)
     present(alert, animated: true, completion: nil)
+  }
+  
+  @IBAction func sliderMoved(_ slider: UISlider) {
+    currentValue = lroundf(slider.value)
   }
 }
 
